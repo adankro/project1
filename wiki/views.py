207af96fb97b22b30d3883d0da179ed09c26060b
Adan <adankro@gmail.com>
2020-08-01 20:16:14 -0500

    agregando templante

--- wiki/views.py
+++ wiki/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
 
+# Create your views here.
 def index(request):
-    return HttpResponse('Hellow from wiki app')
+    return render(request,'test.html')
 
